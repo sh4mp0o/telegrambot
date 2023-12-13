49c9a63977058a2bc25f29a04d213b7763e1a53e
Матвей Николаев <matvey.nikolaev.200355@gmail.com>
2023-12-13 18:53:31 +0300

    you can customize the schedule

--- telegrambot/Check.cs
+++ telegrambot/Check.cs
@@ -11,28 +11,35 @@ namespace telegrambot
     {
         private static List<Client>? _client;
         private static SerializationOfClient serializationOfClient = new SerializationOfClient(new JSONSerialization());
-        static public string KeyboardDay(int i)
+        static public string KeyboardDay(int j)
         {
             _client = serializationOfClient.Deserialization();
+            Schedule schedule = new Schedule();
             DateTime _date = new DateTime();
-            _date = DateTime.Today.AddDays(i);
-            int count = _client.FindAll(x => x.DateTime.Day == DateTime.Today.AddDays(i).Day).Count;
-            if (_date.DayOfWeek == DayOfWeek.Sunday || _date.DayOfWeek == DayOfWeek.Saturday)
+            _date = DateTime.Today.AddDays(j);
+            int count = _client.FindAll(x => x.DateTime.Day == DateTime.Today.AddDays(j).Day).Count;
+            if (count < schedule.timetable[_date.DayOfWeek].Length)
             {
-                if (count < 3)
-                {
-                    return $"true {DateTime.Today.AddDays(i).Day}.{DateTime.Today.Month}";
-                }
-                return "false";
+                return $"true {DateTime.Today.AddDays(j).Day}.{DateTime.Today.Month}";
             }
-            else
-            {
-                if (count <= 0)
-                {
-                    return $"true  {DateTime.Today.AddDays(i).Day}.{DateTime.Today.Month}";
-                }
-                return "false";
-            }
+            return "false";
+
+            //if (_date.DayOfWeek == DayOfWeek.Sunday || _date.DayOfWeek == DayOfWeek.Saturday)
+            //{
+            //    if (count < 3)
+            //    {
+            //        return $"true {DateTime.Today.AddDays(j).Day}.{DateTime.Today.Month}";
+            //    }
+            //    return "false";
+            //}
+            //else
+            //{
+            //    if (count <= 0)
+            //    {
+            //        return $"true  {DateTime.Today.AddDays(j).Day}.{DateTime.Today.Month}";
+            //    }
+            //    return "false";
+            //}
         }
         static public InlineKeyboardMarkup KeyboardDays()
         {
@@ -40,63 +47,80 @@ namespace telegrambot
             {
                 new List<InlineKeyboardButton>()
             };
+            int rows = 0;
             for (int i = 1; i < 4; i++)
             {
                 if (KeyboardDay(i).Split().First() == "true")
                 {
-                    list[0].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardDay(i).Split().Last()}", $"day {i}"));
+                    list[rows].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardDay(i).Split().Last()}", $"day {i}"));
                 }
             }
+            rows++;
             list.Add(new List<InlineKeyboardButton>());
             for (int i = 4; i < 8; i++)
             {
                 if (KeyboardDay(i).Split().First() == "true")
                 {
-                    list[1].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardDay(i).Split().Last()}", $"day {i}"));
+                    list[rows].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardDay(i).Split().Last()}", $"day {i}"));
                 }
             }
+            rows++;
             list.Add(new List<InlineKeyboardButton>());
-            list[2].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backDays"));
+            list[rows].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backDays"));
             return new InlineKeyboardMarkup(list);
         }
         static public InlineKeyboardMarkup KeyboardTimes(long id, List<Client> clients)
         {
             DateTime _date = new DateTime();
+            Schedule schedule = new Schedule();
             DateTime _dateTime = new DateTime(2023,10,24,10,0,0);
             _client = serializationOfClient.Deserialization();
             _date = clients.Find(x => x.Id == id).DateTime;
+            int count = schedule.timetable[_date.DayOfWeek].Length;
             List<List<InlineKeyboardButton>> list = new List<List<InlineKeyboardButton>>();
-            if (_date.DayOfWeek == DayOfWeek.Sunday || _date.DayOfWeek == DayOfWeek.Saturday)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    list.Add(new List<InlineKeyboardButton>());
-                    if (KeyboardTime(_dateTime.AddHours(i*4).Hour.ToString(), _date).Split().First() == "true")
-                    {
-                        list[i].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardTime(_dateTime.AddHours(i * 4).Hour.ToString(), _date).Split().Last()}:00", $"time {KeyboardTime(_dateTime.AddHours(i * 4).Hour.ToString(), _date).Split().Last()}:00"));
-                    }
-                }
-                list.Add(new List<InlineKeyboardButton>());
-                list[3].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backTime"));
-                return new InlineKeyboardMarkup(list);
-            }
-            else
+            for (int i = 0; i < count; i++)
             {
                 list.Add(new List<InlineKeyboardButton>());
-                if (KeyboardTime("18:00", _date).Split().First() == "true")
+                if (KeyboardTime(schedule.timetable[_date.DayOfWeek][i], _date).Split().First() == "true")
                 {
-                    list[0].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardTime("18:00", _date).Split().Last()}", $"time {KeyboardTime("18:00", _date).Split().Last()}"));
+                    list[i].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardTime(schedule.timetable[_date.DayOfWeek][i], _date).Split().Last()}", $"time {KeyboardTime(schedule.timetable[_date.DayOfWeek][i], _date).Split().Last()}"));
                 }
-                list.Add(new List<InlineKeyboardButton>());
-                list[1].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backTime"));
-                return new InlineKeyboardMarkup(list);
             }
+            list.Add(new List<InlineKeyboardButton>());
+            list[count].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backTime"));
+            return new InlineKeyboardMarkup(list);
+
+            //if (_date.DayOfWeek == DayOfWeek.Sunday || _date.DayOfWeek == DayOfWeek.Saturday)
+            //{
+            //    for (int i = 0; i < 3; i++)
+            //    {
+            //        list.Add(new List<InlineKeyboardButton>());
+            //        if (KeyboardTime(_dateTime.AddHours(i*4).Hour.ToString(), _date).Split().First() == "true")
+            //        {
+            //            list[i].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardTime(_dateTime.AddHours(i * 4).Hour.ToString(), _date).Split().Last()}:00", $"time {KeyboardTime(_dateTime.AddHours(i * 4).Hour.ToString(), _date).Split().Last()}:00"));
+            //        }
+            //    }
+            //    list.Add(new List<InlineKeyboardButton>());
+            //    list[3].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backTime"));
+            //    return new InlineKeyboardMarkup(list);
+            //}
+            //else
+            //{
+            //    list.Add(new List<InlineKeyboardButton>());
+            //    if (KeyboardTime("18:00", _date).Split().First() == "true")
+            //    {
+            //        list[0].Add(InlineKeyboardButton.WithCallbackData($"{KeyboardTime("18:00", _date).Split().Last()}", $"time {KeyboardTime("18:00", _date).Split().Last()}"));
+            //    }
+            //    list.Add(new List<InlineKeyboardButton>());
+            //    list[1].Add(InlineKeyboardButton.WithCallbackData("Назад ◀️", "backTime"));
+            //    return new InlineKeyboardMarkup(list);
+            //}
 
         }
         static public string KeyboardTime(string time,DateTime date)
         {
             _client = serializationOfClient.Deserialization();
-            if (_client.Exists(x => x.DateTime.Day == date.Day && x.Time == time+":00"))
+            if (_client.Exists(x => x.DateTime.Day == date.Day && x.Time == time))
             {
                 return "false";
             }
--- telegrambot/Schedule.cs
+++ telegrambot/Schedule.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace telegrambot
 {
-    internal class Schedule
+    public class Schedule
     {
+        public Dictionary<DayOfWeek, string[]> timetable = new Dictionary<DayOfWeek, string[]>();
+        public Schedule()
+        {
+            string[] weekdays = { "18:00" };
+            string[] weekend = { "10:00", "14:00", "18:00" };
+            timetable.Add(DayOfWeek.Monday, weekdays);
+            timetable.Add(DayOfWeek.Tuesday, weekdays);
+            timetable.Add(DayOfWeek.Wednesday, weekdays);
+            timetable.Add(DayOfWeek.Thursday, weekdays);
+            timetable.Add(DayOfWeek.Friday, new string[2] {"13:00","19:00" });
+            timetable.Add(DayOfWeek.Saturday, weekend);
+            timetable.Add(DayOfWeek.Sunday, weekend);
+        }
     }
 }
